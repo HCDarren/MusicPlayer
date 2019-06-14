fe4dc91d2ac56bf5063cadbe6e98b03d53e625f6
HCDarren <240336124@qq.com>
2019-06-14 23:11:18 +0800

    init music player

---
+++ app/src/main/java/com/darren/music/player/MainActivity.java
@@ -0,0 +1,35 @@
+package com.darren.music.player;
+
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    File mMusicFile = new File(Environment.getExternalStorageDirectory(),"BaiduNetdisk/我的资源/0乔布斯传（完结0/017第十三章 制造Mac：过程就是奖励（上）.mp3");
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+
+        Log.e("TAG","file is exist: "+mMusicFile.exists());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
